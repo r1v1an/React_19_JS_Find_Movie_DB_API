2c29b914a3bde7cdffc9758d3b1286b7c20546bb
r1v1an <rivrivian@gmail.com>
2025-05-13 15:23:19 +0500

    day_0

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,13 @@
-const Card = ({title, rating, isCool, actors}) => {
+const Card = ({title, rating, isCool}) => {
   return(
-    <div>
+    <div style={{
+      border: '1px solid white',
+      padding: '20px',
+      margin: '10px',
+      backgroundColor: '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px'
+    }}>
       <h2>{title} {rating} {isCool ? "Cool" : "terrible"}</h2>
     </div>
   )
@@ -10,12 +17,12 @@ const Card = ({title, rating, isCool, actors}) => {
 const App = () => {
 
     return (
-      <>
-      <h2>Functional Arrow Component</h2>
-      <Card title="Star Wars" rating={5} isCool={true} actors={[{name:'R2D2'}]}/>
-      <Card title="Avatat" rating={4} isCool={false} actors={[{name:'Blueman'}]}/>
-      <Card title="Lion king" rating={3} isCool={true} actors={[{name:'Simba'}]}/>
-      </>
+      <div className="card-container">
+        <h2>Functional Arrow Component</h2>
+        <Card title="Star Wars" rating={5} isCool={true}/>
+        <Card title="Avatat" rating={4} isCool={false}/>
+        <Card title="Lion king" rating={3} isCool={true}/>
+      </div>
     )
 }
 
