f9b0633d416d9862fe0033fe8039098365a59504
r1v1an <rivrivian@gmail.com>
2025-05-13 15:58:02 +0500

    day_0

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,29 +1,29 @@
-const Card = ({title}) => {
-  return(
-    <div style={{
-      border: '1px solid white',
-      padding: '20px',
-      margin: '10px',
-      backgroundColor: '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px'
-    }}>
-      <h2>{title}</h2>
-    </div>
-  )
-}
+import { useState } from "react";
 
+const Card = ({ title }) => {
+
+  const [hasLiked, setHasLiked] = useState(false); // Хук деструктуирует массив[логическая переменная, обновление состояния этой переменной]
+
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(true)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
+    </div>
+  );
+};
 
 const App = () => {
 
-    return (
-      <div className="card-container">
-        <h2>Functional Arrow Component</h2>
-        <Card title="Star Wars" rating={5} isCool={true}/>
-        <Card title="Avatat"/>
-        <Card title="Lion king"/>
-      </div>
-    )
-}
+  return (
+    <div className="card-container">
+      <Card title="Star Wars" rating={5} isCool={true} />
+      <Card title="Avatat" />
+      <Card title="The Lion king" />
+    </div>
+  );
+};
 
-export default App
+export default App;
