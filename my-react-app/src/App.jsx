e9c916d4839b73e72a1f958271f440d86f1ed4f9
r1v1an <rivrivian@gmail.com>
2025-05-16 12:58:46 +0500

    day_1_tailwind_installed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern"/>
+
+    </main>
   );
 };
 
