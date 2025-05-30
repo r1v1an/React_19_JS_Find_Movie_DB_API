bb414ce05fbf175fecb0ddf84685ef1d9230f58b
r1v1an <rivrivian@gmail.com>
2025-05-30 16:06:31 +0500

    day_4_MovieCard

--- my-react-app/src/components/MovieCard.jsx
+++ my-react-app/src/components/MovieCard.jsx
@@ -1,12 +1,31 @@
 const MovieCard = ({
-  movie: { title, vote_average, poster_path, relase_date, russian_language },
+  movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
   return (
     <div className="movie-card">
-      <p className="text-white">{title}</p>
-      {/* key=id не нужен т.к. он уже есть в <MovieCard/> */}
-    </div>
-  );
-};
+        <img src={poster_path ? 
+                `https://image.tmdb.org/t/p/w500/${poster_path}}` : '/no-movie.png'} 
+             alt={title}/>
+             
+        <div className="mt-4">
+            <h3>{title}</h3>
 
-export default MovieCard;
+            <div className="content">
+                <div className="rating">
+                    <img src="star-img.svg" alt="Star icon" />
+                    <p>{vote_average ? vote_average.toFixed(2) : 'N/A'}</p>
+                </div>
+
+                <span>•</span> {/* dott symbol alt + 0149 */}
+                <p className="lang">{original_language}</p>
+
+                <span>•</span>
+                <p className="year">{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MovieCard
