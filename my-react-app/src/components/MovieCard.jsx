4d807568ca11828123a6ac87db6869063206b3ae
r1v1an <rivrivian@gmail.com>
2025-09-10 17:44:21 +0500

    day_16_small-fix

--- my-react-app/src/components/MovieCard.jsx
+++ my-react-app/src/components/MovieCard.jsx
@@ -25,8 +25,7 @@ const MovieCard = ({
           <button className="favorite-btn" onClick={onFavoriteClick}>
             {favorite ? "❤️" : "🤍"}
           </button>
-        </div>     
-             alt={title}/>   
+        </div>
         <div className="mt-4">
             <h3>{title}</h3>
 
